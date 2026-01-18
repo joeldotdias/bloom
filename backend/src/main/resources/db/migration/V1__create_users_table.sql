69fc47df3b441f9a133c4cfa7c69b6de19fa330b
joeldotdias <joeldias0204@gmail.com>
2026-01-18 15:10:20 +0530

    chore(backend): add users table migration

---
+++ backend/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL CHECK ( username ~ '^[a-zA-Z0-9_]{3,50}$' ),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    pfp TEXT,
+    bio TEXT,
+    role VARCHAR(50) NOT NULL DEFAULT 'USER',
+    is_banned BOOLEAN DEFAULT FALSE,
+    is_online BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
